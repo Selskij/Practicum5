b5329c6dcef6912271fffcca71f9ac73ec960ac2
Selskij <selskij.v@edu.spbstu.ru>
2022-05-26 20:35:10 +0300

    cache done

---
+++ docstring.py
@@ -0,0 +1,19 @@
+import json
+
+from faker import Faker
+
+from config import ARGUM1
+
+
+print(ARGUM1+10)
+
+def test(arg1,arg2):
+    """
+    This function ....
+    :param arg1: Argum1 ....
+    :param arg2:
+    :return:
+    """
+    ...
+print(test.__name__)
+print(test.__doc__)
